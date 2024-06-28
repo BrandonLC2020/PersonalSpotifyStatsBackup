e6405d174d07810ada351824455c697d7aae8566
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-06-27 20:56:44 -0400

    Update DatabaseManager.py

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -3,5 +3,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="brandon@100.64.138.178",
   user="brandon",
-  password="MyServer22!"
+  password="MyServer22!",
+  database="mydatabase"
 )
+
+mycursor = mydb.cursor()
