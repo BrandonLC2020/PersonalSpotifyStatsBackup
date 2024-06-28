391e29f0a20e8801dec6cd7945129b98deead4fe
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-06-28 18:02:48 -0400

    created a environment file

--- SpotifyAPIManager.py
+++ SpotifyAPIManager.py
@@ -4,3 +4,12 @@ def get_access_token():
     url = 'https://accounts.spotify.com/api/token'
     headers = { 'Content-Type' : 'application/x-www-form-urlencoded' }
     data = 'grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret'
+
+    access_token_request = requests.post(url, data=data, headers=headers)
+    print(access_token_request.text)
+
+def main():
+    get_access_token()
+
+if __name__ == '__main__':
+    main()
