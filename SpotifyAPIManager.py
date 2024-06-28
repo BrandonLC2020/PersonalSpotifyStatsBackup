e9c2e1f93a4b7fb34deb937aa69ae794761bf8cc
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-06-28 18:28:09 -0400

    Update SpotifyAPIManager.py

--- SpotifyAPIManager.py
+++ SpotifyAPIManager.py
@@ -1,12 +1,15 @@
 import requests
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 def get_access_token(): 
     url = 'https://accounts.spotify.com/api/token'
     headers = { 'Content-Type' : 'application/x-www-form-urlencoded' }
-    data = 'grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret'
+    data = f"grant_type=client_credentials&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}"
 
     access_token_request = requests.post(url, data=data, headers=headers)
     print(access_token_request.text)
