aef8cbc2ffeae28af0e78bbba10f4854c1491d59
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-06-28 17:22:17 -0400

    Update SpotifyAPIManager.py

--- SpotifyAPIManager.py
+++ SpotifyAPIManager.py
@@ -2,3 +2,5 @@ import requests
 
 def get_access_token(): 
     url = 'https://accounts.spotify.com/api/token'
+    headers = { 'Content-Type' : 'application/x-www-form-urlencoded' }
+    data = 'grant_type=client_credentials&client_id=your-client-id&client_secret=your-client-secret'
