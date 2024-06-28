f206229d93b795d16ef49fbc373ff0e7938492d2
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-06-28 19:19:45 -0400

    successful access token retrieval

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -1,13 +1,18 @@
 import mysql.connector
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_DB = os.getenv('DATABASE_DB')
 
 mydb = mysql.connector.connect(
-  host="",
-  user="",
-  password="",
-  database=""
+  host=DATABASE_HOST,
+  user=DATABASE_USERNAME,
+  password=DATABASE_PASSWORD,
+  database=DATABASE_DB
 )
 
 mycursor = mydb.cursor()
--- SpotifyAPIManager.py
+++ SpotifyAPIManager.py
@@ -10,9 +10,19 @@ def get_access_token():
     url = 'https://accounts.spotify.com/api/token'
     headers = { 'Content-Type' : 'application/x-www-form-urlencoded' }
     data = f"grant_type=client_credentials&client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}"
+    try:
+        access_token_response = requests.post(url, data=data, headers=headers)
 
-    access_token_request = requests.post(url, data=data, headers=headers)
-    print(access_token_request.text)
+        if access_token_response.status_code == 200:
+            access_token_json = access_token_response.json()
+            access_token = access_token_json['access_token']
+            return access_token
+        else:
+            print('Error:', access_token_response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print('Error:', e)
+        return None
 
 def main():
     get_access_token()
