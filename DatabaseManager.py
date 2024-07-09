d04a1b8e482b8d7b5c1bc0bc084142a9254abdc2
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-07-08 20:54:01 -0400

    Update DatabaseManager.py

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -8,11 +8,13 @@ DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 DATABASE_DB = os.getenv('DATABASE_DB')
 
-mydb = mysql.connector.connect(
-  host=DATABASE_HOST,
-  user=DATABASE_USERNAME,
-  password=DATABASE_PASSWORD,
-  database=DATABASE_DB
-)
+class DatabaseManager:
 
-mycursor = mydb.cursor()
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host=DATABASE_HOST,
+            user=DATABASE_USERNAME,
+            password=DATABASE_PASSWORD,
+            database=DATABASE_DB
+        )
+        self.mycursor = self.mydb.cursor()
