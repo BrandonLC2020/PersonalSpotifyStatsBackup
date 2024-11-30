d13daf7c3370aa980f30d4801f814cbf34069880
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-11-30 18:22:07 -0500

    Update runBackup.sh

--- runBackup.sh
+++ runBackup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+python3 -m venv SpotifyStatsEnv
+source SpotifyStatsEnv/bin/activate
+
+pip3 install -r requirements.txt
+
 xterm -e python SpotifyAuthorizationManager.py
 xterm -e ngrok http --domain=boss-bluegill-intimate.ngrok-free.app 3000
 
-python main.py
+python3 main.py
+
+deactivate
