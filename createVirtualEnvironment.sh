2f2a9fe1469714a7450140e7782d20769466d446
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-11-30 19:28:04 -0500

    updated scripts

--- createVirtualEnvironment.sh
+++ createVirtualEnvironment.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 python3 -m venv SpotifyStatsEnv
+
+source SpotifyStatsEnv/bin/activate
+
+pip3 install -r requirements.txt
+
+deactivate
--- runBackup.sh
+++ runBackup.sh
@@ -2,8 +2,6 @@
 
 source SpotifyStatsEnv/bin/activate
 
-pip3 install -r requirements.txt
-
 xterm -e python SpotifyAuthorizationManager.py
 xterm -e ngrok http --domain=boss-bluegill-intimate.ngrok-free.app 3000
 
