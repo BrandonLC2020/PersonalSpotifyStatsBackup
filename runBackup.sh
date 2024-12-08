7c8912b3529a3e23c355a5b740ea3c1abe6d7aac
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-12-08 10:32:40 -0500

    updating more!

--- runBackup.sh
+++ runBackup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Load environment variables from .env file
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+fi
+
 source SpotifyStatsEnv/bin/activate
 
 xterm -e python3 SpotifyAuthorizationManager.py
-xterm -e ngrok http --domain=boss-bluegill-intimate.ngrok-free.app 8081
+xterm -e ngrok http --domain="$NGROK_DOMAIN" "$NGROK_PORT"
 
 python3 main.py
 
